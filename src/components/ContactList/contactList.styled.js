53baa8584943ff46a81984eb609886fc06b0ce3b
Katerina-Riabukh <katrysya7773@gmail.com>
2023-09-03 12:26:39 +0200

    contactList added

---
+++ src/components/ContactList/contactList.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const UL = styled.ul`
+  margin-right: auto;
+  margin-top: 0;
+  margin-bottom: 0;
+  height: 270px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  list-style: none;
+  padding: 0;
+`;
