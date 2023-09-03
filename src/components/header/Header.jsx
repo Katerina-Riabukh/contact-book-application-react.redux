423397b10e7841511588ac2ee80fef06ee86129c
Katerina-Riabukh <katrysya7773@gmail.com>
2023-09-03 11:55:30 +0200

    usermenu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,10 @@
 import { NavUser } from 'components/header/Nav-User/NavUser';
 import { Outlet } from 'react-router-dom';
-import { UserMenu } from 'components/User/UserMenu';
 import { useSelector } from 'react-redux';
 import { getIsLoggedIn } from 'redux/user/userSelectors';
 import { Container } from './Header.styled';
 import { NavBar } from './Nav-Bar/NavBar';
+import { UserMenu } from 'components/User/UserMenu';
 
 export const Header = () => {
   const isLoggedIn = useSelector(getIsLoggedIn);
