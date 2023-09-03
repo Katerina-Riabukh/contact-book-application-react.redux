51fa6afc5ab3706824175d1d330316804cd022d3
Katerina-Riabukh <katrysya7773@gmail.com>
2023-09-03 11:59:04 +0200

    replased folders

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,9 @@
-import { NavUser } from 'components/header/Nav-User/NavUser';
+import { NavUser } from 'components/Nav-User/NavUser';
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { getIsLoggedIn } from 'redux/user/userSelectors';
 import { Container } from './Header.styled';
-import { NavBar } from './Nav-Bar/NavBar';
+import { NavBar } from '../Nav-Bar/NavBar';
 import { UserMenu } from 'components/User/UserMenu';
 
 export const Header = () => {
